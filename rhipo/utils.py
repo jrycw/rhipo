21b74660733498859847ba20349ef4b22652568c
jrycw <jerry@ycwu.space>
2024-05-02 01:44:33 +0800

    add docs for cols_merge_as_str

--- rhipo/utils.py
+++ rhipo/utils.py
@@ -8,6 +8,29 @@ def _exprize(elem: pl.Expr | str):
 
 
 def cols_merge_as_str(*elems: pl.Expr | str, alias: str = "merged_col") -> pl.Expr:
+    """
+    Parameters
+    ----------
+    elems
+        str or Polars expressions.
+    alias
+        alias for the final Polars expressions.
+
+    Returns:
+    ----------
+    pl.Expr
+        Polars expressions
+
+    Examples
+    --------
+    ```{python}
+    import polars as pl
+    from rhipo.utils import cols_merge_as_str
+
+    df = pl.DataFrame({"col1": ["a"], "col2": ["b"]})
+    (df.select(cols_merge_as_str(pl.col("col1"), "^_^", pl.col("col2"))))
+    ```
+    """
     if not elems:
         raise ValueError("No expressions are given")
     cols = None
